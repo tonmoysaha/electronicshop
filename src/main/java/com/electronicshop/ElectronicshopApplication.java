bad38b721c4572f28711f0bc13f39692f8acd7e7
tonmoysaha <tonmoysahaopi@gmail.com>
2019-08-28 13:43:17 +0600

    initial commit eshop

---
+++ src/main/java/com/electronicshop/ElectronicshopApplication.java
@@ -0,0 +1,13 @@
+package com.electronicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicshopApplication.class, args);
+	}
+
+}
