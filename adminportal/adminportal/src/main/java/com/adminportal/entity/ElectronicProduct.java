44d60cf507dcd96e11c5ca1b6363f3aeccda3614
tonmoysaha <tonmoysahaopi@gmail.com>
2019-09-19 07:25:30 +0600

    adding electronicproduct entity and commonheader template

--- adminportal/adminportal/src/main/java/com/adminportal/entity/ElectronicProduct.java
+++ adminportal/adminportal/src/main/java/com/adminportal/entity/ElectronicProduct.java
@@ -1,8 +1,143 @@
 package com.adminportal.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 public class ElectronicProduct {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String model;
+	private String brandName;
+	private String productCode;
+	private String category;
+	
+	private double shippingWeight;
+	private double listPrice;
+	private double ourPrice;
+	private boolean active = true;
+	
+	@Column(columnDefinition = "text")
+	private String description;
+	private int inStockNumber;
+	
+	private MultipartFile image;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getShippingWeight() {
+		return shippingWeight;
+	}
+
+	public void setShippingWeight(double shippingWeight) {
+		this.shippingWeight = shippingWeight;
+	}
+
+	public double getListPrice() {
+		return listPrice;
+	}
+
+	public void setListPrice(double listPrice) {
+		this.listPrice = listPrice;
+	}
+
+	public double getOurPrice() {
+		return ourPrice;
+	}
+
+	public void setOurPrice(double ourPrice) {
+		this.ourPrice = ourPrice;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getInStockNumber() {
+		return inStockNumber;
+	}
+
+	public void setInStockNumber(int inStockNumber) {
+		this.inStockNumber = inStockNumber;
+	}
+
+	public MultipartFile getImage() {
+		return image;
+	}
+
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+	
+	
+	
+	
 
 }
